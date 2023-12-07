d84ab77ef1210b385bb31502a7d50d15a51361be
James Stephenson <12j4mes@gmail.com>
2023-12-07 19:19:55 +0000

    answers here and there

--- 2/answer.py
+++ 2/answer.py
@@ -1,21 +1,31 @@
 import re
-from typing import Dict
 
 
-def part_one(filepath: str, max_balls: Dict[str, int]):
+def part_one(filepath: str) -> int:
     idx_sum = 0
     pattern = r"(\d+)\s(blue|red|green)"
+    max_balls = {"red": 12, "green": 13, "blue": 14}
+
     with open(filepath) as file:
         for idx, line in enumerate(file):
-            row_possible = True
-            balls = {"red": 0, "blue": 0, "green": 0}
+            game_possible = True
             catches = re.findall(pattern, line)
             for count, ball_color in catches:
-                if row_possible:
-                    balls[ball_color] += int(count)
-                    row_possible = balls[ball_color] <= max_balls[ball_color]
+                if game_possible:
+                    game_possible = int(count) <= max_balls[ball_color]
 
-            if row_possible:
+                if not game_possible:
+                    break
+
+            if game_possible:
                 idx_sum += idx + 1
 
     return idx_sum
+
+
+def part_two(filepath: str) -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    part_one("2/test.txt")
