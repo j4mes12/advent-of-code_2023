eb68e08361d413eab00aa42050bdcf972e1e6717
James Stephenson <12j4mes@gmail.com>
2023-12-08 22:59:03 +0000

    updates to day 3

--- 3/answer.py
+++ 3/answer.py
@@ -7,36 +7,48 @@ def part_one(filepath: str) -> int:
 
     with open(filepath) as file:
         for line_num, line in enumerate(file):
-            digit_matches = re.finditer(r"\d+", line)
-            symbol_matches = re.finditer(r"[^\.\d\s]", line)
             digits_coords = [
-                (match.group(), match.start(), match.end()) for match in digit_matches
+                (match.group(), match.start(), match.end())
+                for match in re.finditer(r"\d+", line)
             ]
-            symbol_coords = [(match.group(), match.start()) for match in symbol_matches]
+            symbol_coords = [match.start() for match in re.finditer(r"[^\.\d\s]", line)]
 
             if symbol_coords:
-                for _, sc in symbol_coords:
+                for sc in symbol_coords:
                     if digits_coords:
                         for num, start, end in digits_coords:
+                            if sc < start - 1:
+                                break
                             if (sc == start - 1 or sc == end) and (num not in record):
                                 sum_val += int(num)
                                 record.append(num)
+                                break
 
-                    if line_num > 0 and prev_digit_coords != []:
+                    if line_num > 0 and prev_digit_coords:
                         for num, start, end in prev_digit_coords:
-                            if sc >= start - 1 and sc <= end and num not in record:
+                            if sc < start - 1:
+                                break
+                            if sc >= start - 1 and sc <= end + 1 and num not in record:
                                 sum_val += int(num)
                                 record.append(num)
+                                break
 
             if digits_coords and line_num > 0 and prev_symbol_coords:
-                for num, start, end in digits_coords:
-                    for _, sc in prev_symbol_coords:
-                        if sc >= start - 1 and sc <= end and num not in record:
+                for sc in prev_symbol_coords:
+                    for num, start, end in digits_coords:
+                        if sc < start - 1:
+                            break
+                        if sc >= start - 1 and sc <= end + 1 and num not in record:
                             sum_val += int(num)
                             record.append(num)
+                            break
 
             # update prev coords of digits
             prev_digit_coords = digits_coords
             prev_symbol_coords = symbol_coords
 
     return sum_val
+
+
+if __name__ == "__main__":
+    part_one("3/test.txt")
